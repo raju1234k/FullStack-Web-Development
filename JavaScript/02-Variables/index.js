a7c33bb4a3a4d748ad32f17127b43bf97143d7eb
Raju Kumar <123308147+raju1234k@users.noreply.github.com>
2024-07-23 14:48:40 +0000

    container config done

---
+++ JavaScript/02-Variables/index.js
@@ -0,0 +1,12 @@
+const regNo=123456789
+let studentName="Raju"
+var branch="CSE"
+Semester="6th"
+
+// Const can naver be changed 
+
+// let may be changed 
+
+// we do not use to var variable due to block and functional scope 
+
+console.table([regNo,studentName,branch,Semester])
