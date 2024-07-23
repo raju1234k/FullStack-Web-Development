d355f7fa03c35ffbecd9744af96a49f91536812b
Raju Kumar <123308147+raju1234k@users.noreply.github.com>
2024-07-23 17:35:49 +0000

    container config done

---
+++ JavaScript/03-Datatypes & Operator/index.js
@@ -0,0 +1,157 @@
+// let score=33 //number 
+
+// let myName="Raju" //String
+
+// let finalAnswer=true //boolean datatype
+
+// console.log(typeof finalAnswer);
+
+// let anss=null //null datatype
+
+// console.log(typeof anss);
+
+// let state; //undefined 
+
+// Arithmetic operator
+// Binary 
+let a=10;
+let b=5;
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(2**2) 
+// ** means Square
+
+// Uninary
+let c=5;
+// preincrement
+console.log(++c) 
+
+// postincrement
+console.log(c++)
+
+let d=9;
+// predecrement
+console.log(--d)
+
+// postdecrement
+console.log(d--)
+
+
+// Assignment operator
+
+let p=10;
+let q=5;
+
+// p=p+2;
+p+=2;
+console.log(p)
+
+// p=p-10;
+p-=10;
+console.log(p)
+
+// Comparison Operator
+
+r=4;
+t=2;
+console.log(t<r)
+
+// Ternary Operator
+
+let agee= 5;
+let status1=(agee>10)? 'i can vote' : 'i can not vote'
+
+console.log(status1)
+
+
+// Logical Operator
+
+// AND operator
+let ans=(true&&true&&true);
+
+console.log(ans)
+
+let ques=(true&&true&&false);
+console.log(ques)
+
+// OR Operator
+let or=(true || true ||true);
+
+console.log(or)
+
+let tr=(false || false ||true);
+
+console.log(tr)
+
+let rs=(false || false ||false);
+
+console.log(rs)
+
+
+// NOT Operator
+
+let name1 =! (true)
+console.log(name1)
+
+let name2 =! (false)
+console.log(name2)
+
+// bitwise  operator
+
+console.log(2&3)
+console.log(2^2)
+
+// if-else condition
+
+let umr =25;
+if(umr>18){
+    console.log('i can vote')
+}
+else{
+    console.log('i can not vote ')
+}
+
+// if-else-if conditons
+
+let num=6;
+if(num==1){
+    console.log('right')
+}
+else if(num==2){
+     console.log('right')
+}
+else if(num==3){
+    console.log('right')
+}
+else if(num==4){
+    console.log('right')
+}
+else if(num==6){
+    console.log('right')
+}
+else(
+    console.log('false')
+)
+
+// Switch statement
+
+let age=3;
+switch(age){
+    case 1:console.log('A')
+    break;
+    case 2:console.log('b')
+    break;
+    case 3:console.log('c')
+    break;
+    case 1:console.log('A')
+    break;
+    case 2:console.log('b')
+    break;
+    case 3:console.log('c')
+}
+
+
+
