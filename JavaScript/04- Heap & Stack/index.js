0369f7fa45ddd199d3df7a26d5f6165766dd96b5
Raju Kumar <123308147+raju1234k@users.noreply.github.com>
2024-07-24 12:00:57 +0000

    jaca script done

---
+++ JavaScript/04- Heap & Stack/index.js
@@ -0,0 +1,12 @@
+let myName="RajuKumar"
+let surName=myName
+console.log(surName);
+
+let userOne ={
+    email:"raju@gmail.com"
+}
+let userTwo=userOne
+userTwo.email="kumar@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
