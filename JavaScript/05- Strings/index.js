ce7fc3faa82c62e043c3e4926653aef75a3f70a3
Raju Kumar <123308147+raju1234k@users.noreply.github.com>
2024-07-25 08:38:13 +0000

    String code done

---
+++ JavaScript/05- Strings/index.js
@@ -0,0 +1,33 @@
+let myName="Raju"
+let MySurname="Jayvant"
+
+//REPLACE 
+// console.log(myName.replace("Raju","ER. RAJU"))
+
+// console.log(myName.includes("Raju"));
+
+let fullName=`${myName} ${MySurname}`
+// console.log(typeof fullName);
+// console.log(fullName.__proto__);
+// console.log(fullName);
+//Constructor of string
+
+// let completeName=new String("RAJU JAYVANT")
+// console.log(completeName[0])
+// console.log(completeName.substring(0,8))
+// console.log(completeName.toLocaleLowerCase());
+// console.log(completeName.__proto__);
+
+// Charactor Position 
+// console.log(completeName.charAt(1));
+
+// console.log(completeName.indexOf('Y'));
+
+// console.log(myName.slice(-7,5))
+
+// TRIM TO CUT EMPTY SPACES 
+
+// let str="       raju       "
+// console.log(str.trim());
+// console.log(str.trimStart())
+// console.log(str.trimEnd());
