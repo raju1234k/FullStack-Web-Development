42aa63890fcf958bbd825d467a34ded71f053d47
Raju Kumar <123308147+raju1234k@users.noreply.github.com>
2024-07-26 12:42:55 +0000

    loop code done

---
+++ JavaScript/07- Loop/index.js
@@ -0,0 +1,80 @@
+
+ // **** FOR LOOP ****
+
+// for(let i=1;i<=6;i++){
+//     console.log(i)
+// }
+// Reverse nomber
+
+// for(let i=10;i>=0;i--){
+//     console.log(i)
+// }
+
+
+// for(let i=1;i<=10;i++){
+//     if(i==5){
+//         console.log('5 printed');
+//         break;
+        
+//     }
+//     console.log(i)
+// }
+
+// for(let i=1;i<=10;i++){
+//     if(i==5){
+       
+//         continue;
+        
+//     }
+//     console.log(i)
+// }
+
+// // **** WHILE LOOP ****
+ 
+// let i=1;
+// while(i<=10){
+//     console.log(i)
+//     i++;
+// }
+
+// let i=10;
+// while(i>=0){
+//     console.log(i)
+//     i--;
+// }
+
+// let i=1;
+// while(i<=5){
+//     if(i==3){
+//       i++;
+//       continue;
+//     }
+//     else
+//         console.log(i)
+//     i++;
+        
+// }
+
+// // ***DO WHILE LOOP ***/
+
+// let i=1;
+// do{
+//   console.log(i)
+//   i++;
+// }while(i<=10)
+
+// for(i=1;i<=10;i++){
+//     console.log(i)
+// }
+
+// let i=1;
+// while(i<=10){
+//     console.log(i)
+//     i++;
+// }
+
+// let i=1;
+// do{
+//  console.log(i)
+//  i++;
+// }while(i<=10)
